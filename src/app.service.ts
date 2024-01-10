ac3b97be5811ccb22c24851a7164eb08479df86d
Abhishek Zade <zabhidoc@gmail.com>
2024-01-10 18:42:28 +0530

    Updated docker-compose

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Abhishek Zade Project';
+    return 'Abhishek Zade Pro';
   }
 }
